b793d5a5ef37588cd45249ac99a7f4be84503c83
JaviCepa <javi@pixelatto.com>
2020-10-05 12:37:07 +0200

    Fixed princess crash on WebGL builds

--- Assets/Princess.cs
+++ Assets/Princess.cs
@@ -12,19 +12,16 @@ public class Princess : MonoBehaviour
         var character = collision.gameObject.GetComponentInParent<Character>();
         if (character != null)
         {
-            UpventureGameManager.instance.character.InputEnabled = false;
-            UpventureGameManager.instance.character.Input = null;
+
             character.CharacterHealth.SetInvulnerable(1000);
             character.CharacterHealth.CurrentHealth = 99999;
 
             var sequence = DOTween.Sequence();
             sequence.AppendCallback(() => transform.localRotation = Quaternion.identity);
-            sequence.Append(transform.DOJump(character.transform.position + new Vector3(0,0.5f,0.1f), 2f, 1, 0.5f));
             sequence.AppendCallback(() => transform.SetParent(character.transform));
-            sequence.AppendInterval(0.5f);
+            sequence.AppendCallback(() => transform.localPosition = new Vector3(0,0.25f,0.1f));
             sequence.Append(character.transform.DOMoveY(3f, 1f).SetRelative(true).SetEase(Ease.OutQuad).OnStart(() => character.transform.DOMoveZ(-5f, 4f).SetRelative(true).SetEase(Ease.Linear)));
             sequence.Append(character.transform.DOMoveY(-3f, 1f).SetRelative(true).SetEase(Ease.InQuad));
-            sequence.AppendInterval(1f);
             sequence.AppendCallback(() => Fader.instance.HideScreen());
             sequence.AppendInterval(1f);
             sequence.AppendCallback(() => UpventureGameManager.instance.PlayEnding());
